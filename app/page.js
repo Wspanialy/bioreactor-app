74246a4ac50ad98c5a38f04e101c03d1b01c3a9c
Kacper <witak.kacper@gmail.com>
2023-11-03 20:16:22 +0100

    visual changes in styles

--- app/page.js
+++ app/page.js
@@ -3,18 +3,18 @@ import ParametersView from "@components/ParametersView"
 
 export default function Home() {
   return (
-    <main className="flex flex-wrap">
-      <div className="w-1/2 p-2">
-        <ParametersView />
+    <main className="flex flex-wrap pt-4 justify-center">
+      <div className="w-1/3 mr-2 p-2">
+        <ParametersView value="6.231" name="pH" channel="2" channelName="2" dateStamp="2023-11-03" />
       </div>
-      <div className="w-1/2 p-2">
-        <ParametersView />
+      <div className="w-1/3 mr-2 p-2">
+      <ParametersView value="6.231" name="pH" channel="2" channelName="2" dateStamp="2023-11-03" />
       </div>
-      <div className="w-1/2 p-2">
-        <ParametersView />
+      <div className="w-1/3 mr-2 p-2">
+      <ParametersView value="6.231" name="pH" channel="2" channelName="2" dateStamp="2023-11-03" />
       </div>
-      <div className="w-1/2 p-2">
-        <ParametersView />
+      <div className="w-1/3 mr-2 p-2">
+      <ParametersView value="6.231" name="pH" channel="2" channelName="2" dateStamp="2023-11-03" />
       </div>
     </main>
   );
--- components/ParametersView.jsx
+++ components/ParametersView.jsx
@@ -1,10 +1,14 @@
-
-const ParametersView = ({params}) => {
+const ParametersView = ({ value, name, channel, channelName, dateStamp }) => {
   return (
-    <div className="border-solid border-2 border-black rounded-3xl">
-    parameters
+    <div className="border-solid border-2 border-black rounded-3xl flex flex-col">
+      <div className="text-center text-2xl mt-2">{value} {name}</div>
+      <div className="m-2 text-sm">
+        <p>Channel: {channel}</p>
+        <p>Name: {channelName}</p>
+        <p>Date: {dateStamp}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ParametersView
+export default ParametersView;
